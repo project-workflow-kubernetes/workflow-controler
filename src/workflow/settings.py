8da084e5c1689eee12adb05af197ef4b7d395f89
liabifano <liabifano@gmail.com>
2018-12-05 18:14:54 +0100

    right path

--- src/workflow/settings.py
+++ src/workflow/settings.py
@@ -8,7 +8,7 @@ RESOURCES_PATH = os.path.join(os.path.abspath(
 
 PERSISTENT_ADDR = os.environ['PERSISTENT_ADDR'] if os.environ.get(
     'PERSISTENT_ADDR', None) else 'localhost:9060'
-VOLUME_PATH = os.environ['VOLUME_PATH'] if os.environ.get('VOLUME_PATH', None) else RESOURCES_PATH
+VOLUME_PATH = os.environ['DATA_PATH'] if os.environ.get('DATA_PATH', None) else RESOURCES_PATH
 ACCESS_KEY = os.environ['MINIO_ACCESS_KEY'] if os.environ.get(
     'MINIO_ACCESS_KEY', None) else 'minio'
 SECRET_KEY = os.environ['MINIO_SECRET_KEY'] if os.environ.get(
